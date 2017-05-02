e3d3cfac86279034ab053ddb6363258e7945fe77
JonnyBoy2000 <jonathon_lokken@yahoo.com>
2017-05-02 13:30:31 -0500

    Update dev.py

--- modules/dev.py
+++ modules/dev.py
@@ -14,5 +14,23 @@ class Dev:
         await self.bot.edit_profile(avatar=avatar)
         await self.bot.say("Changed avatar.")
         
+    def _list_cogs(self):
+        cogs = [os.path.basename(f) for f in glob.glob("modules/*.py")]
+        return ["modules." + os.path.splitext(f)[0] for f in cogs]
+
+    @commands.command(pass_context=True)
+    @checks.is_owner()
+    async def modules(self, ctx):
+        """Shows Brooklyns modules."""
+        loaded = [c.__module__.split(".") for c in self.bot.cogs.values()]
+        unloaded = [c.split(".") for c in self._list_cogs()
+                    if c.split(".") not in loaded]
+        if not unloaded:
+            unloaded = ["None"]
+        msg=discord.Embed(description="**Showing modules for Brooklyn.**", colour=discord.Colour(value=colour))
+        msg.add_field(name="Loaded", value="{}".format(", ".join(sorted(loaded))))
+        msg.add_field(name="Unloaded", value="{}".format(", ".join(sorted(unloaded))))
+        await self.bot.say(embed=msg)
+
 def setup(bot):
     bot.add_cog(Dev(bot))
