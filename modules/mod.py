2b611ba81257df090b3f5c7be8a49582ae287f9f
JonnyBoy2000 <jonathon_lokken@yahoo.com>
2017-05-02 14:19:11 -0500

    Update mod.py

--- modules/mod.py
+++ modules/mod.py
@@ -15,7 +15,7 @@ class Mod:
         return m.author.id == self.bot.user.id
 
     @commands.command(pass_context=True)
-    @commands.check(check_roles)
+    @commands.botcom()
     async def clean(self, ctx, *, limit:int=100):
         """Allows the bot to delete his own messages"""
         counter = await ctx.message.channel.purge(limit = limit,check=self.delete_mine)
