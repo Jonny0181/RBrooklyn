3f3072adc0d2624eb845353c9f95d536d71f1667
JonnyBoy2000 <jonathon_lokken@yahoo.com>
2017-05-02 01:23:15 -0500

    Update mod.py

--- modules/mod.py
+++ modules/mod.py
@@ -2,11 +2,104 @@ import discord
 import asyncio
 from utils import checks
 from discord.ext import commands
+from utils.dataIO import dataIO
 
 class Mod:
     def __init__(self, bot):
         self.bot = bot
         self._tmp_banned_cache = []
+        self.settings = dataIO.load_json("data/warner/warnings.json")
+        
+    @commands.command(pass_context=True, no_pm=True)
+    @checks.botcom()
+    async def warn(self, ctx, user:discord.Member, times:int=1):
+        """Warn people for their actions."""
+        serverid = ctx.message.server.id
+        userid = user.id
+        if serverid not in self.settings:
+            self.settings[serverid] = {}
+            self.save_settings()
+        if userid not in self.settings[serverid]:
+            self.settings[serverid][userid] = 1
+            self.save_settings()
+            await self.bot.say("The user has 1 warnings but nothing happens yet, next up: 5 minute mute.")
+            return
+        else:
+            self.settings[serverid][userid] += times
+            self.save_settings()
+            if self.settings[serverid][userid] == 1:
+                await self.bot.say("The user has 1 warnings but nothing happens yet, next up: 5 minute mute.")
+            if self.settings[serverid][userid] == 2:
+                await self.bot.say("The user has 2 warnings and has been muted for 5 minutes, next up: 30 minute mute.")
+                await self.mute(user, 5)
+            elif self.settings[serverid][userid] == 3:
+                await self.bot.say("The user has 3 warnings and has been muted for 30 minutes, next up: kick.")
+                await self.mute(user, 30)
+            elif self.settings[serverid][userid] == 4:
+                try:
+                    await self.bot.kick(user)
+                    await self.bot.say("The user has 4 warnings and has been kicked, next up: ban.")
+                except discord.Forbidden:
+                    await self.bot.say("The user has 4 warnings but could not be kicked because I do not have the right perms for that.")
+                except:
+                    await self.bot.say("The user has 4 warnings but an unknown error occured while trying to kick the user.")
+            elif self.settings[serverid][userid] >= 5:
+                try:
+                    await self.bot.ban(user, delete_message_days=3)
+                    del self.settings[serverid][userid]
+                    self.save_settings()
+                    await self.bot.say("The user has 5 warnings and has been banned.")
+                except discord.Forbidden:
+                    await self.bot.say("The user has 5 warnings but could not be banned because I do not have the right perms for that.")
+                except:
+                    await self.bot.say("The user has 5 warnings but an unknown error occured while trying to ban the user.")
+                
+    @commands.command(pass_context=True, no_pm=True)
+    @checks.botcom()
+    async def resetwarns(self, ctx, user:discord.Member):
+        """Reset the warnings you gave to someone"""
+        serverid = ctx.message.server.id
+        userid = user.id
+        if serverid not in self.settings:
+            await self.bot.say("No one in this server has got a warning yet.")
+            return
+        elif userid not in self.settings[serverid]:
+            await self.bot.say("This user doesn't have a warning yet.")
+            return
+        else:
+            del self.settings[serverid][userid]
+            self.save_settings()
+            await self.bot.say("Users warnings succesfully reset!")
+            return
+            
+    @commands.command(pass_context=True, no_pm=True)
+    async def warns(self, ctx, user:discord.Member):
+        """See how much warnings someone has."""
+        if ctx.message.server.id not in self.settings:
+            await self.bot.say("No one in this server has got a warning yet.")
+            return
+        elif user.id not in self.settings[ctx.message.server.id]:
+            await self.bot.say("This user doesn't have a warning yet.")
+            return
+        elif self.settings[ctx.message.server.id][user.id] == 1:
+            await self.bot.say("This user has {} warning.".format(self.settings[ctx.message.server.id][user.id]))
+            return
+        else:
+            await self.bot.say("This user has {} warnings.".format(self.settings[ctx.message.server.id][user.id]))
+            
+    def save_settings(self):
+        dataIO.save_json("data/warner/warnings.json", self.settings)
+            
+    async def mute(self, member, minutes:int):
+        for channel in member.server.channels:
+            perms = discord.PermissionOverwrite()
+            perms.send_messages = False
+            await self.bot.edit_channel_permissions(channel, member, perms)
+        await asyncio.sleep(minutes * 60)
+        for channel in member.server.channels:
+            perms = discord.PermissionOverwrite()
+            perms.send_messages = None
+            await self.bot.edit_channel_permissions(channel, member, perms)
         
     @commands.command(pass_context = True)
     @checks.botcom()
@@ -118,9 +211,18 @@ class Mod:
             except:
                 pass
 
+def check_folders():
+    if not os.path.exists("data/warner"):
+        print("Creating data/warner folder...")
+        os.makedirs("data/warner")
 
-
+def check_files():
+    if not os.path.exists("data/warner/warnings.json"):
+        print("Creating data/warner/warnings.json file...")
+        dataIO.save_json("data/warner/warnings.json", {})
                 
 def setup(bot):
-	n = Mod(bot)
-	bot.add_cog(n)
+    check_folders()
+    check_files()
+    n = Mod(bot)
+    bot.add_cog(n)
