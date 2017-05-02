826139f6563f2da72ebf37f0db9b7091bddaa4a0
JonnyBoy2000 <jonathon_lokken@yahoo.com>
2017-05-02 00:14:24 -0500

    Update info.py

--- modules/info.py
+++ modules/info.py
@@ -1,4 +1,11 @@
 import discord
+import random
+import os
+import asyncio
+import psutil
+import datetime
+import time
+import copy
 from discord.ext import commands
 
 class Info:
@@ -12,6 +19,53 @@ class Info:
         time = (msg.timestamp - ctx.message.timestamp).total_seconds() * 1000
         await self.bot.edit_message(msg, 'Pong: {}ms :ping_pong:'.format(round(time)))
         
+    @commands.command(pass_context=True)
+    async def stats(self, ctx):
+        """Shows stats."""
+        text_channels = 0
+        voice_channels = 0
+        list2 = []
+        list = []
+        for i in self.bot.servers:
+            if i.me.voice_channel is not None:
+                list.append(i.me.voice_channel)
+        for c in list:
+            list2.extend(c.voice_members)
+        mem_v = psutil.virtual_memory()
+        cpu_p = psutil.cpu_percent(interval=None, percpu=True)
+        cpu_usage = sum(cpu_p)/len(cpu_p)
+        online = len([e.name for e in self.bot.get_all_members() if not e.bot and e.status == discord.Status.online])
+        idle = len([e.name for e in self.bot.get_all_members() if not e.bot and e.status == discord.Status.idle])
+        dnd = len([e.name for e in self.bot.get_all_members() if not e.bot and e.status == discord.Status.dnd])
+        offline = len([e.name for e in self.bot.get_all_members() if not e.bot and e.status == discord.Status.offline])
+        seconds = time.time() - starttime
+        m, s = divmod(seconds, 60)
+        h, m = divmod(m, 60)
+        d, h = divmod(h, 24)
+        w, d = divmod(d, 7)
+        t1 = time.perf_counter()
+        await self.bot.type()
+        t2 = time.perf_counter()
+        data = discord.Embed(description="Showing stats for {}.".format(self.bot.user.name), colour=discord.Colour.red())
+        data.add_field(name="Owner", value="<@146040787891781632>")
+        data.add_field(name="Ping", value="{}ms".format(round((t2-t1)*1000)))
+	data.add_field(name="Shard ID", value=self.bot.shard_id)
+	data.add_field(name="Shard Count", value=self.bot.shard_count)
+        data.add_field(name="Servers", value=len(self.bot.servers))
+        data.add_field(name="Api version", value=discord.__version__)
+        data.add_field(name="Users", value="{} Online<:vpOnline:212789758110334977>\n{} Idle<:vpAway:212789859071426561>\n{} Dnd<:vpDnD:236744731088912384>\n{} Offline<:vpOffline:212790005943369728>".format(online, idle, dnd, offline))
+        data.add_field(name="Channels", value="{} Voice Channels\n{} Text Channels".format(len([e for e in self.bot.get_all_channels() if e.type == discord.ChannelType.voice]), len([e for e in self.bot.get_all_channels() if e.type == discord.ChannelType.text])))
+        data.add_field(name='CPU usage', value='{0:.1f}%'.format(cpu_usage))
+        data.add_field(name='Memory usage', value='{0:.1f}%'.format(mem_v.percent))
+        data.add_field(name="Commands", value="{0} active modules, with {1} commands...".format(len(self.bot.cogs), len(self.bot.commands)))
+        data.add_field(name='Uptime', value="%d Weeks," % (w) + " %d Days," % (d) + " %d Hours,"
+                                   % (
+                h) + " %d Minutes," % (m) + " and %d Seconds!" % (s))
+        data.add_field(name="Voice Stats:", value="Connected to {} voice channels, with a total of {} users.".format(len(list), len(list2)), inline=False)
+        data.set_author(name=ctx.message.author.display_name, icon_url=ctx.message.author.avatar_url)
+        data.set_thumbnail(url=ctx.message.author.avatar_url)
+        await self.bot.say(embed=data)
+
     @commands.command(pass_context=True)
     async def mods(self, ctx):
         """Shows mods in the server."""
