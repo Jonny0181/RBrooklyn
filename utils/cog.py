a0381380bda824bc1e83050d63b58b3336818c06
JonnyBoy2000 <jonathon_lokken@yahoo.com>
2017-05-28 21:02:17 +0000

    Create cog.py

---
+++ utils/cog.py
@@ -0,0 +1,9 @@
+
+"""Definition of the bot's barebones cog class."""
+
+class Cog:
+    """Base cog module. To be used as a template."""
+    def __init__(self, bot):
+        self.bot = bot
+        self.logger = bot.logger
+        self.loop = bot.loop
